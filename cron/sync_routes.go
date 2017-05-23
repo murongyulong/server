e5373dae40ebb5829c9740819b994b8f9d8f652b
murongyulong <931357304@qq.com>
2017-05-23 09:45:01 +0800

    Update sync_routes.go

--- cron/sync_routes.go
+++ cron/sync_routes.go
@@ -2,7 +2,7 @@ package cron
 
 import (
 	"fmt"
-	"github.com/smartcaas/common/model"
+	"github.com/murongyulong/common/model"
 	"github.com/murongyulong/server/g"
 	"github.com/garyburd/redigo/redis"
 	"log"
