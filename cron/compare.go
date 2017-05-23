a0dddfe75572f7db68e0d2b4bb6b563c62de571b
murongyulong <931357304@qq.com>
2017-05-23 16:26:43 +0800

    Update compare.go

--- cron/compare.go
+++ cron/compare.go
@@ -283,7 +283,7 @@ func DockerRun(app *model.App, ip string) {
 		Config: &docker.Config{
 			Memory: int64(app.Memory * 1024 * 1024),
 			ExposedPorts: map[docker.Port]struct{}{
-				docker.Port(port): {},
+				docker.Port("80/tcp"): {},
 			},
 			Image:        app.Image,
 			AttachStdin:  false,
@@ -294,7 +294,7 @@ func DockerRun(app *model.App, ip string) {
 		},
 		HostConfig: &docker.HostConfig{
 			PortBindings: map[docker.Port][]docker.PortBinding{
-				port: []docker.PortBinding{docker.PortBinding{}},
+				"80/tcp": []docker.PortBinding{docker.PortBinding{}},
 			},
 		},
 	}
@@ -331,7 +331,7 @@ func DockerRun(app *model.App, ip string) {
 
 	err = client.StartContainer(container.ID, &docker.HostConfig{
 		PortBindings: map[docker.Port][]docker.PortBinding{
-			port: []docker.PortBinding{docker.PortBinding{}},
+			"80/tcp": []docker.PortBinding{docker.PortBinding{}},
 		},
 	})
 
