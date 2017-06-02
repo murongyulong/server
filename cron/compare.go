b387504ca022953e4a7d08c972e26a8cb03abd30
murongyulong <931357304@qq.com>
2017-06-02 15:08:16 +0800

    Update compare.go

--- cron/compare.go
+++ cron/compare.go
@@ -277,15 +277,15 @@ func DockerRun(app *model.App, ip string) {
 		return
 	}
 	//动态加载用户指定端口
-	//port := fmt.Sprintf("%s/tcp", app.Port)
+	port := fmt.Sprintf("%s/tcp", app.Port)
 	type Port string
-	Port port= fmt.Sprintf("%s/tcp", app.Port)
+	Port p= port
 
 	opts := docker.CreateContainerOptions{
 		Config: &docker.Config{
 			Memory: int64(app.Memory * 1024 * 1024),
 			ExposedPorts: map[docker.Port]struct{}{
-				docker.Port(port): {},
+				docker.Port(p): {},
 			},
 			Image:        app.Image,
 			AttachStdin:  false,
@@ -296,7 +296,7 @@ func DockerRun(app *model.App, ip string) {
 		},
 		HostConfig: &docker.HostConfig{
 			PortBindings: map[docker.Port][]docker.PortBinding{
-				port: []docker.PortBinding{docker.PortBinding{}},//"80/tcp"与port有什么区别呢?
+				p: []docker.PortBinding{docker.PortBinding{}},//"80/tcp"与port有什么区别呢?
 			},
 		},
 	}
@@ -333,7 +333,7 @@ func DockerRun(app *model.App, ip string) {
 
 	err = client.StartContainer(container.ID, &docker.HostConfig{
 		PortBindings: map[docker.Port][]docker.PortBinding{
-			port: []docker.PortBinding{docker.PortBinding{}},
+			p: []docker.PortBinding{docker.PortBinding{}},
 		},
 	})
 
