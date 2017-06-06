25eabdbc3ff08cf82cee3415cffed473c263faaf
murongyulong <931357304@qq.com>
2017-06-06 11:55:10 +0800

    Update compare.go

--- cron/compare.go
+++ cron/compare.go
@@ -291,6 +291,9 @@ func DockerRun(app *model.App, ip string) {
 			AttachStdout: false,
 			AttachStderr: false,
 			Env:          BuildEnvArray(envVars),
+			Volumes: map[string]struct{}{
+				"root/dinp/data/"+app.Name: {},
+			},
 		},
 		HostConfig: &docker.HostConfig{
 			PortBindings: map[string][]docker.PortBinding{
